f0e7984c23fb8e8215a94bcc02aa70e34fda22ba
preginald <peter@reginald.com.au>
2021-10-25 08:52:44 +1100

    Add test and dashboard routes

--- src/router.ts
+++ src/router.ts
@@ -9,6 +9,18 @@ const routes: Array<RouteRecordRaw> = [
     component: () => import("./views/Home.vue"),
   },
   {
+    path: "/dashboard",
+    alias: "/dashboard",
+    name: "dashboard",
+    component: () => import("./views/Dashboard/Home.vue"),
+  },
+  {
+    path: "/dashboard/chain",
+    alias: "/dashboard/chain",
+    name: "chain",
+    component: () => import("./views/Dashboard/Chain.vue"),
+  },
+  {
     path: "/calculators/simple/",
     name: "simple-calculator",
     component: () => import("./components/calculators/SimpleCalculator.vue"),
@@ -17,6 +29,11 @@ const routes: Array<RouteRecordRaw> = [
     path: "/calculators/yield-farming-breakeven",
     name: "yield-farming-breakeven-calculator",
     component: () => import("./components/calculators/YieldFarmBreakeven.vue"),
+  },
+  {
+    path: "/test",
+    name: "test",
+    component: () => import("./components/tests/Test.vue"),
   },
 ];
 
